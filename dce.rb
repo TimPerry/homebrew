b9768cc45c8d6139c96e4c4cad6bdcf75092bbf4
Tim Perry <tim.perry@bbc.co.uk>
2016-11-04 16:04:36 +0000

    Updating dce install

--- dce.rb
+++ dce.rb
@@ -1,9 +1,9 @@
 class Dce < Formula
   desc "Docker Compose Exec task runner"
   homepage "https://github.com/TimPerry/dce"
-  url "https://github.com/TimPerry/dce"
+  url "https://github.com/TimPerry/dce/archive/master.zip"
   version "1.0.0"
-  sha256 "9749d30904b5c71c74653fa4da8bb1a8edc6b11538415dbb0912f82e8dbdb454"
+  sha256 "ef03b7016533d289cbf18c3026ba070927d274145fb785db300442f3fb9dd236"
 
   def install
     bin.install "dce"
