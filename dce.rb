46649108d64123397c858ebb260e3d0d7a8bfa47
Tim Perry <tim.perry@bbc.co.uk>
2016-11-04 15:29:19 +0000

    Moving to use brew commands for dce bin install

--- dce.rb
+++ dce.rb
@@ -1,10 +1,11 @@
 class Dce < Formula
   desc "Docker Compose Exec task runner"
   homepage "https://github.com/TimPerry/dce"
-  url "https://github.com/TimPerry/dce.git"
+  url "https://github.com/TimPerry/dce"
   version "1.0.0"
+  sha256 "9749d30904b5c71c74653fa4da8bb1a8edc6b11538415dbb0912f82e8dbdb454"
 
   def install
-    cp "dce", "/usr/local/bin/dce"
+    bin.install "dce"
   end
 end
